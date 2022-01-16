4c19d91fac469f945facf2dbd1832dcb22c6f6ca
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2022-01-17 02:39:56 +0530

    LSP keymaps -> which-key mod

--- lua/ps/lsp/init.lua
+++ lua/ps/lsp/init.lua
@@ -1,3 +1,2 @@
 require"ps.lsp.lspinstaller"
 require"ps.lsp.handlers".setup()
-require"ps.lsp.keymaps"
--- lua/ps/which-key/which-key-mappings.lua
+++ lua/ps/which-key/which-key-mappings.lua
@@ -12,6 +12,7 @@ local nopts = {
 
 local mappings = {
   ["q"] = { "<cmd>q!<CR>", "Quit" },
+
   -- Files
   f = {
     name = "File";
@@ -25,7 +26,7 @@ local mappings = {
   b = {
     name = "Buffers";
     b = { "<cmd>Telescope buffers<cr>", "Open Buffers" },
-    d = { "<cmd>Bdelete!<cr>", "Close Buffer" },
+    d = { "<cmd>bdelete!<cr>", "Close Buffer" },
   },
 
   -- Help
@@ -71,6 +72,39 @@ local mappings = {
     d = {
       "<cmd>Gitsigns diffthis HEAD<cr>",
       "Diff",
+    },
+  },
+
+  -- LSP
+  l = {
+    name = "LSP",
+    a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+    d = {
+      "<cmd>Telescope lsp_document_diagnostics<cr>",
+      "Document Diagnostics",
+    },
+    w = {
+      "<cmd>Telescope lsp_workspace_diagnostics<cr>",
+      "Workspace Diagnostics",
+    },
+    f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
+    i = { "<cmd>LspInfo<cr>", "Info" },
+    I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+    j = {
+      "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
+      "Next Diagnostic",
+    },
+    k = {
+      "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>",
+      "Prev Diagnostic",
+    },
+    l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+    q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+    r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+    S = {
+      "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+      "Workspace Symbols",
     },
   },
 
