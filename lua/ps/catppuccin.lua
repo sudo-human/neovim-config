31ffbba9fd31419da81dd626a4edb13cf6628507
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2022-01-24 23:34:00 +0530

    Yes Comments

---
+++ lua/ps/catppuccin.lua
@@ -0,0 +1,13 @@
+local status, catppuccin = pcall(require, "catppuccin")
+if not status then return end
+
+catppuccin.setup {
+  term_colors = true,
+  integration = { nvimtree = {
+      enabled = true,
+      show_root = true,
+      transparent_panel = true,
+    },
+    which_key = true
+  }
+}
--- lua/ps/plugins.lua
+++ lua/ps/plugins.lua
@@ -45,7 +45,6 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-  use "numToStr/Comment.nvim" -- Easily comment stuff
   use "kyazdani42/nvim-web-devicons"
   use "lewis6991/impatient.nvim"
   use {
@@ -72,7 +71,10 @@ return packer.startup(function(use)
   -- Themes
   use({
     "catppuccin/nvim",
-    as = "catppuccin"
+    as = "catppuccin",
+    config = function ()
+      require "ps.catppuccin"
+    end
   })
 
   -- Lualine
