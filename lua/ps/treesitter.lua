24f25be3d411c7765e3e8b09c06c59e6014a976f
Prateek Sharma <prateek.sharma@simplifyvms.com>
2022-01-25 19:24:16 +0530

    Fold and indent fix, fold is not working with telescope

--- lua/ps/treesitter.lua
+++ lua/ps/treesitter.lua
@@ -15,9 +15,18 @@ configs.setup {
     disable = { "" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
-  indent = { enable = true, disable = { "yaml" } },
+  -- indent = { enable = true, disable = { "yaml" } },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
   },
 }
