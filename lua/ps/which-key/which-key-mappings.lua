a5d22c779444f53556a005c4270b9eaf3a6a4de9
Prateek Sharma <prateek.sharma@simplifyvms.com>
2022-02-16 11:27:14 +0530

    Use fd

--- lua/ps/which-key/which-key-mappings.lua
+++ lua/ps/which-key/which-key-mappings.lua
@@ -20,7 +20,7 @@ local mappings = {
   f = {
     name = "File";
     -- f = { "<cmd>Telescope fd<cr>", "Find Files" },
-    f = { "<cmd>lua require'telescope.builtin'.find_files({ file_ignore_patterns = {'^node_modules/', '^.git/', '^.cache', '%.o', '%.a', '%.out', '%.class', '%.pdf', '%.mkv', '%.mp4', '%.zip'}})<cr>", "Find Files" },
+    f = { "<cmd>lua require'telescope.builtin'.fd({ file_ignore_patterns = {'^node_modules/', '^.git/', '^.cache', '%.o', '%.a', '%.out', '%.class', '%.pdf', '%.mkv', '%.mp4', '%.zip'}})<cr>", "Find Files" },
     r = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
     g = { "<cmd>Telescope git_files<cr>", "Open git tracked files" },
     s = { "<cmd>w!<cr>", "Save file" },
