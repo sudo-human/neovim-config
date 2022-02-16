bc0007d3f97885d9fe3668b04199fd031d66336a
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2022-02-16 06:17:33 +0530

    Disable neogit disable_builtin_notifications

--- lua/ps/neogit.lua
+++ lua/ps/neogit.lua
@@ -2,6 +2,7 @@ local status, neogit = pcall(require, "neogit")
 if not status then return end
 
 neogit.setup {
+  disable_builtin_notifications = false,
   integrations = {
     diffview = true
   },
