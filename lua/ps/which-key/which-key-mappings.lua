c45f9405619c95220700d1a581fc6a1b0add4a64
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2022-02-16 11:41:12 +0530

    Nah slow telescope, bad boy telescope

--- lua/ps/which-key/which-key-mappings.lua
+++ lua/ps/which-key/which-key-mappings.lua
@@ -19,8 +19,7 @@ local mappings = {
   -- Files
   f = {
     name = "File";
-    -- f = { "<cmd>Telescope fd<cr>", "Find Files" },
-    f = { "<cmd>lua require'telescope.builtin'.fd({ file_ignore_patterns = {'^node_modules/', '^.git/', '^.cache', '%.o', '%.a', '%.out', '%.class', '%.pdf', '%.mkv', '%.mp4', '%.zip'}})<cr>", "Find Files" },
+    f = { "<cmd>Telescope fd<cr>", "Find Files" },
     r = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
     g = { "<cmd>Telescope git_files<cr>", "Open git tracked files" },
     s = { "<cmd>w!<cr>", "Save file" },
