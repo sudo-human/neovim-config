25f87db3a69d4e84bc53b54a635dfa8345249295
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2022-02-24 23:33:52 +0530

    Finally accepting diagnostic signs

--- lua/ps/lsp/handlers.lua
+++ lua/ps/lsp/handlers.lua
@@ -1,11 +1,24 @@
 local M = {}
 
 M.setup = function()
+  local signs = {
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn", text = "" },
+    { name = "DiagnosticSignHint", text = "" },
+    { name = "DiagnosticSignInfo", text = "" },
+  }
+
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  end
   vim.diagnostic.config{
     virtual_text = false,
     update_in_insert = true,
     underline = true,
     severity_sort = true,
+    signs = {
+      active = signs,
+    },
     float = {
       focusable = false,
       style = "minimal",
