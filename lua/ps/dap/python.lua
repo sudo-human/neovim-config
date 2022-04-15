d5dd7871ce24c5c151366651358c71139cd705f7
Prateek Sharma <prateek.sharma@simplifyvms.com>
2022-04-16 00:13:20 +0530

    Fixed cmp docs and debug config

--- lua/ps/dap/python.lua
+++ lua/ps/dap/python.lua
@@ -50,7 +50,32 @@ dap.configurations.python = {
     args = {
         "runserver",
         "127.0.0.1:8000",
-        "--noreload",
+      },
+    django = true,
+    justMyCode = true,
+  },
+  {
+    name = "Python: Django shell_plus",
+    type = "python",
+    request = "launch",
+    program = "${workspaceFolder}/manage.py",
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/svmsenv/bin/python') == 1 then
+        return cwd .. '/svmsenv/bin/python'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+      else
+        return '/usr/bin/python'
+      end
+    end;
+    args = {
+        "shell_plus",
+        "127.0.0.1:8000",
+        -- "--noreload",
       },
     django = true,
     justMyCode = true,
