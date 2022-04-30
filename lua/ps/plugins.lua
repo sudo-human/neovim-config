ad77eea9d0ad28b5a0fee1201b15f99b33c1a057
Prateek Sharma <ps.prateek.sharma143@gamil.com>
2022-04-30 17:18:28 +0530

    Bring order

--- lua/ps/plugins.lua
+++ lua/ps/plugins.lua
@@ -138,11 +138,11 @@ return packer.startup(function(use)
   }
 
   -- Complition
+  use "hrsh7th/nvim-cmp"
   use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-buffer"
   use "hrsh7th/cmp-path"
   use "hrsh7th/cmp-cmdline"
-  use "hrsh7th/nvim-cmp"
   use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
   use "hrsh7th/cmp-vsnip"
   use "onsails/lspkind-nvim"
