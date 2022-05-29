5fca1c4f7275672093a5a59d020d72092330ff63
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2022-05-29 23:21:45 +0530

    catppuccin updated setup

--- lua/ps/catppuccin.lua
+++ lua/ps/catppuccin.lua
@@ -2,13 +2,71 @@ local status, catppuccin = pcall(require, "catppuccin")
 if not status then return end
 
 catppuccin.setup {
+  transparent_background = false,
   term_colors = true,
-  integration = {
+  styles = {
+    comments = "italic",
+    conditionals = "italic",
+    loops = "NONE",
+    functions = "NONE",
+    keywords = "NONE",
+    strings = "NONE",
+    variables = "NONE",
+    numbers = "NONE",
+    booleans = "NONE",
+    properties = "NONE",
+    types = "NONE",
+    operators = "NONE",
+  },
+  integrations = {
+    treesitter = true,
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = "italic",
+        hints = "italic",
+        warnings = "italic",
+        information = "italic",
+      },
+      underlines = {
+        errors = "underline",
+        hints = "underline",
+        warnings = "underline",
+        information = "underline",
+      },
+    },
+    lsp_trouble = false,
+    cmp = true,
+    lsp_saga = false,
+    gitgutter = false,
+    gitsigns = true,
+    telescope = true,
     nvimtree = {
       enabled = true,
-      show_root = true,
-      transparent_panel = true,
+      show_root = false,
+      transparent_panel = false,
     },
-    which_key = true
-  }
-}
+    neotree = {
+      enabled = false,
+      show_root = false,
+      transparent_panel = false,
+    },
+    which_key = false,
+    indent_blankline = {
+      enabled = true,
+      colored_indent_levels = false,
+    },
+    dashboard = true,
+    neogit = true,
+    vim_sneak = false,
+    fern = false,
+    barbar = false,
+    bufferline = true,
+    markdown = true,
+    lightspeed = false,
+    ts_rainbow = false,
+    hop = false,
+    notify = true,
+    telekasten = true,
+    symbols_outline = true,
+  }}
