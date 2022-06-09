44208c32121c12266032f854269d93a15db41fdb
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2022-06-09 13:37:30 +0530

    diffview things yet to be done

--- lua/ps/diffview.lua
+++ lua/ps/diffview.lua
@@ -29,19 +29,29 @@ diffview.setup {
     },
   },
   file_history_panel = {
+    log_options = {
+      single_file = {
+        max_count = 512,
+        follow = true,
+      },
+      multi_file = {
+        max_count = 128,
+        -- follow = false   -- `follow` only applies to single-file history
+      },
+    },
     win_config = {
       position = "left",
       width = 35,
       height = 16,
     },
-    log_options = {
-      max_count = 256,      -- Limit the number of commits
-      follow = false,       -- Follow renames (only for single file)
-      all = false,          -- Include all refs under 'refs/' including HEAD
-      merges = false,       -- List only merge commits
-      no_merges = false,    -- List no merge commits
-      reverse = false,      -- List commits in reverse order
-    },
+    -- log_options = {
+    --   max_count = 256,      -- Limit the number of commits
+    --   follow = false,       -- Follow renames (only for single file)
+    --   all = false,          -- Include all refs under 'refs/' including HEAD
+    --   merges = false,       -- List only merge commits
+    --   no_merges = false,    -- List no merge commits
+    --   reverse = false,      -- List commits in reverse order
+    -- },
   },
   default_args = {    -- Default args prepended to the arg-list for the listed commands
     DiffviewOpen = {},
