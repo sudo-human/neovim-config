67070727adffb22910770100eb7190d7e3b959d4
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2022-07-05 11:00:52 +0530

    Neorg setup

--- lua/ps/neorg.lua
+++ lua/ps/neorg.lua
@@ -1,8 +1,12 @@
 require('neorg').setup {
     load = {
         ["core.defaults"] = {},
-        ["core.neorg.concealer"] = {},
-        ["core.norg.completion"] = {},
+        -- ["core.neorg.concealer"] = {},
+        ["core.norg.completion"] = {
+          config = {
+            engine = "nvim-cmp"
+          }
+        },
         ["core.integrations.nvim-cmp"] = {},
         ["core.integrations.treesitter"] = {},
         ["core.ui"] = {},
